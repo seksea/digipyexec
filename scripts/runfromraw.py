3ac53e95f5f65b9c6860f8a4b3d7fd9db239ca63
whyireland <whyireland@protonmail.com>
2020-11-06 12:01:33 +0000

    first script example

---
+++ scripts/runfromraw.py
@@ -0,0 +1,8 @@
+# import urllib.request as r, os
+# f=open('pscript.py', 'wb')
+# f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/digipyexec.ino').read())
+# f.close()
+# os.system("python.exe pscript.py")
+
+# USE THIS:
+import urllib.request as r, os;f=open('pscript.py', 'wb');f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/digipyexec.ino').read());f.close();os.system("python.exe pscript.py")
