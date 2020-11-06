2950369489ff4e5b8f37a2b387df132b3048eb33
whyireland <whyireland@protonmail.com>
2020-11-06 17:16:29 +0000

    updated with shorter link

--- scripts/runfromraw.py
+++ scripts/runfromraw.py
@@ -1,12 +1,11 @@
 # A script that downloads another python script from the internet and runs it,
-# allows you to have extremely large scripts with slower typing times
+# allows you to have extremely large scripts with faster typing times
 
-#   Cleaner formatted version:
-# import urllib.request as r, os
-# f=open('pscript.py', 'wb')
-# f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read())
-# f.close()
-# os.system('python.exe pscript.py')
+# LINK CANNOT BE TOO LONG or the dspark will kill itself and show as an unrecognized device, but a pastebin link works perfect
 
-#   One line ver (use this):
-import urllib.request as r, os;f=open('pscript.py', 'wb');f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read());f.close();os.system('python.exe pscript.py')
+#define PY_SCRIPT \
+  DigiKeyboard.print("import urllib.request as r, os;"); \
+  DigiKeyboard.print("f=open('pscript.py', 'wb');"); \
+  DigiKeyboard.print("f.write(r.urlopen('https://pastebin.com/raw/FtY9kquy').read());"); \
+  DigiKeyboard.print("f.close();"); \
+  DigiKeyboard.print("os.system('python.exe pscript.py')");
