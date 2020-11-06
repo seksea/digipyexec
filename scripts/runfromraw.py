e495ef93c3c894ffd454ad8b6b24ebb58920f86b
whyireland <whyireland@protonmail.com>
2020-11-06 12:04:49 +0000

    added description

--- scripts/runfromraw.py
+++ scripts/runfromraw.py
@@ -1,8 +1,12 @@
+# A script that downloads another python script from the internet and runs it,
+# allows you to have extremely large scripts with slower typing times
+
+#   Cleaner formatted version:
 # import urllib.request as r, os
 # f=open('pscript.py', 'wb')
 # f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read())
 # f.close()
 # os.system("python.exe pscript.py")
 
-# USE THIS:
+#   One line ver (use this):
 import urllib.request as r, os;f=open('pscript.py', 'wb');f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read());f.close();os.system("python.exe pscript.py")
