d49980520452cebfbfd261b8e1b052a1e50487d2
whyireland <whyireland@protonmail.com>
2020-11-06 12:02:29 +0000

    updated with test script

--- scripts/runfromraw.py
+++ scripts/runfromraw.py
@@ -1,8 +1,8 @@
 # import urllib.request as r, os
 # f=open('pscript.py', 'wb')
-# f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/digipyexec.ino').read())
+# f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read())
 # f.close()
 # os.system("python.exe pscript.py")
 
 # USE THIS:
-import urllib.request as r, os;f=open('pscript.py', 'wb');f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/digipyexec.ino').read());f.close();os.system("python.exe pscript.py")
+import urllib.request as r, os;f=open('pscript.py', 'wb');f.write(r.urlopen('https://raw.githubusercontent.com/whyire/digipyexec/main/scripts/testscript.py').read());f.close();os.system("python.exe pscript.py")
